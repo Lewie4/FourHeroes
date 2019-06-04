6f3f9830e1859a37dd516e0029ac3bc07bc2b8ec
Lewis Nicholson <Lewies@Live.co.uk>
2019-06-04 20:26:52 +0100

    Character stats are now curves

--- FourHeroes/Assets/Scripts/Classes/ClassData.cs
+++ FourHeroes/Assets/Scripts/Classes/ClassData.cs
@@ -18,15 +18,15 @@ public class ClassData : ScriptableObject
 {
     [Header("Primary Stats")]
     [SerializeField] CharacterClass m_class;
-    [SerializeField] float m_health;
-    [SerializeField] float m_strength;
-    [SerializeField] float m_dexterity;
-    [SerializeField] float m_intelligence;
+    [SerializeField] AnimationCurve m_health;
+    [SerializeField] AnimationCurve m_strength;
+    [SerializeField] AnimationCurve m_dexterity;
+    [SerializeField] AnimationCurve m_intelligence;
     [SerializeField] ClassStatMultiplier m_multiplier;
 
     [Header("Secondary Stats")]
-    [SerializeField] float m_crit;
-    [SerializeField] float m_dodge;
-    [SerializeField] float m_block;
-    [SerializeField] float m_attackSpeed;
+    [SerializeField] AnimationCurve m_crit;
+    [SerializeField] AnimationCurve m_dodge;
+    [SerializeField] AnimationCurve m_block;
+    [SerializeField] AnimationCurve m_attackSpeed;
 }
