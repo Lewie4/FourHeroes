ecf88c0f8c4aeb37310f7feb18053eb024a9ad39
Lewis Nicholson <Lewies@Live.co.uk>
2019-06-06 19:31:23 +0100

    Adding game manager

---
+++ FourHeroes/Assets/Scripts/GameManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private static string PLAYERSCENE = "Player";
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (m_instance == null)
+            {
+                m_instance = FindObjectOfType<GameManager>();
+            }
+            return m_instance;
+        }
+    }
+
+    private static GameManager m_instance;
+
+    private Group m_playerGroup;
+
+    private void Awake()
+    {
+        if (m_instance != null)
+        {
+            Destroy(this);
+        }
+        m_instance = this;
+    }
+
+    private void Start()
+    {
+        if (m_playerGroup == null)
+        {
+            SceneManager.LoadSceneAsync(PLAYERSCENE, LoadSceneMode.Additive);
+        }
+    }
+
+    public void RegisterPlayerGroup(PlayerGroup playerGroup)
+    {
+        if (m_playerGroup == null)
+        {
+            m_playerGroup = playerGroup;
+        }
+        else
+        {
+            Destroy(playerGroup);
+        }
+    }
+
+    void Update()
+    {
+
+    }
+}
--- FourHeroes/Assets/Scripts/Group.cs
+++ FourHeroes/Assets/Scripts/Group.cs
@@ -4,4 +4,18 @@ using UnityEngine;
 
 public class Group : MonoBehaviour
 {
+    public const int GROUPSIZE = 4;
+
+    [SerializeField] private Hero[] m_group = new Hero[GROUPSIZE];
+
+    private void Update()
+    {
+        for (int i = 0; i < GROUPSIZE; i++)
+        {
+            if (m_group[i] != null)
+            {
+                m_group[i].TryAttack();
+            }
+        }
+    }
 }
