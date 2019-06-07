fee0568d91c036131bac7a8baeb668e4c810a237
Lewis Nicholson <Lewies@Live.co.uk>
2019-06-07 23:59:59 +0100

    Final item changes?

--- FourHeroes/Assets/Scripts/Items/BaseItem.cs
+++ FourHeroes/Assets/Scripts/Items/BaseItem.cs
@@ -32,31 +32,10 @@ public enum Stat
 }
 
 [System.Serializable]
-public class StatPossibility
-{
-    public bool health;
-    public bool strength;
-    public bool dexterity;
-    public bool intelligence;
-
-    public bool crit;
-    public bool dodge;
-    public bool block;
-    public bool attackSpeed;
-}
-
-[System.Serializable]
 public class BaseItem : ScriptableObject
 {
     public ItemType itemType;
     public ItemQuality quality;
     public ItemStatMultiplier statMultiplier;
-
-    [Header("Item Stats")]
-    public StatPossibility stat1;
-    public StatPossibility stat2;
-    public StatPossibility stat3;
-    public StatPossibility stat4;
-    public int specialAbility;   //TODO: Replace with special ability (Mainly for Relics)
-
+    public StatPossibilities statPossibilities;
 }
---
+++ FourHeroes/Assets/Scripts/Items/StatPossibilities.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class StatPossibility
+{
+    public bool health;
+    public bool strength;
+    public bool dexterity;
+    public bool intelligence;
+
+    public bool crit;
+    public bool dodge;
+    public bool block;
+    public bool attackSpeed;
+}
+
+[CreateAssetMenu(fileName = "StatPossibilities1", menuName = "Items/Stat Possibilities")]
+public class StatPossibilities : ScriptableObject
+{
+    public StatPossibility stat1;
+    public StatPossibility stat2;
+    public StatPossibility stat3;
+    public StatPossibility stat4;
+}
