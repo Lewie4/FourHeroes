d1428cc18fa8b9ecd26d60165753cb4918d91888
Lewis Nicholson <Lewies@Live.co.uk>
2019-06-07 23:35:10 +0100

    Removing quality from item stat multiplier

--- FourHeroes/Assets/Scripts/Items/ItemStatMultiplier.cs
+++ FourHeroes/Assets/Scripts/Items/ItemStatMultiplier.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "ItemStatMult", menuName = "Items/Multiplier")]
 public class ItemStatMultiplier : ScriptableObject
 {
-    public ItemQuality quality;
     public StatMultiplier statMultiplier1;
     public StatMultiplier statMultiplier2;
     public StatMultiplier statMultiplier3;
