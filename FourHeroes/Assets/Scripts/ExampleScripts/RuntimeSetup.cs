e20b50f3a85b7fab595efed9f7000949326debd7
Lewis Nicholson <Lewies@Live.co.uk>
2019-06-27 20:08:55 +0100

    Test cosmetics

--- FourHeroes/Assets/Scripts/ExampleScripts/RuntimeSetup.cs
+++ FourHeroes/Assets/Scripts/ExampleScripts/RuntimeSetup.cs
@@ -8,10 +8,15 @@ public class RuntimeSetup : MonoBehaviour
 {
     public Character Character;
 
+    public void Start()
+    {
+        EquipArmor("Slave");
+    }
+
     /// <summary>
     /// Example call: SetBody("HeadScar", "Basic", "Human", "Basic");
     /// </summary>
-    public void SetBody(string headName, string headCollection, string bodyName, string bodyCollection)
+    public void SetBody(string headName, string bodyName)
     {
         var head = SpriteCollection.Instance.Head.Single(i => i.cosmeticName == headName);
         var body = SpriteCollection.Instance.Body.Single(i => i.cosmeticName == bodyName);
@@ -19,7 +24,7 @@ public class RuntimeSetup : MonoBehaviour
         Character.SetBody(head.sprites[0], body.sprites);
     }
 
-    public void EquipMeleeWeapon1H(string sname, string collection)
+    public void EquipMeleeWeapon1H(string sname)
     {
         var entry = SpriteCollection.Instance.MeleeWeapon1H.Single(i => i.cosmeticName == sname);
 
@@ -27,35 +32,35 @@ public class RuntimeSetup : MonoBehaviour
         Character.EquipMeleeWeapon(entry.sprites[0], entry.sprites[1]);
     }
 
-    public void EquipMeleeWeapon2H(string sname, string collection)
+    public void EquipMeleeWeapon2H(string sname)
     {
         var entry = SpriteCollection.Instance.MeleeWeapon2H.Single(i => i.cosmeticName == sname);
 
         Character.EquipMeleeWeapon(entry.sprites[0], entry.sprites[0], true);
     }
 
-    public void EquipBow(string sname, string collection)
+    public void EquipBow(string sname)
     {
         var entry = SpriteCollection.Instance.Bow.Single(i => i.cosmeticName == sname);
 
         Character.EquipBow(entry.sprites);
     }
 
-    public void EquipShield(string sname, string collection)
+    public void EquipShield(string sname)
     {
         var entry = SpriteCollection.Instance.Shield.Single(i => i.cosmeticName == sname);
 
         Character.EquipShield(entry.sprites[0]);
     }
 
-    public void EquipArmor(string sname, string collection)
+    public void EquipArmor(string sname)
     {
         var entry = SpriteCollection.Instance.Armor.Single(i => i.cosmeticName == sname);
 
         Character.EquipArmor(entry.sprites);
     }
 
-    public void EquipHelmet(string sname, string collection)
+    public void EquipHelmet(string sname)
     {
         var entry = SpriteCollection.Instance.Helmet.Single(i => i.cosmeticName == sname);
 
